4b8be832a7abce350d3f94512b61e329167a4afe
Satya Saranu <Satya.Saranu@local.com>
2021-01-12 20:07:13 -0600

    Revert " boto3 configuration file for us-west-1"
    This reverts commit e32482239cb9c55cc50d5bf5f41f244c0ed1b4b5.

--- ec2_boto3.py
+++ ec2_boto3.py
@@ -1,12 +1,12 @@
 import boto3
 
-ec2 = boto3.resource('ec2', region_name='us-west-1')
+ec2 = boto3.resource('ec2')
 
 # create a new EC2 instance
 instances = ec2.create_instances(
-     ImageId='ami-03130878b60947df3',  
+     ImageId='ami-0be2609ba883822ec',
      MinCount=1,
-     MaxCount=1,
+     MaxCount=2,
      InstanceType='t2.micro',
      KeyName='dev2020'
  )
